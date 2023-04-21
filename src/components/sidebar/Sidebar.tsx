16a3de7cb561f6775d91043a2635e0136c0cf24e
hnegishi <hnegishimaru@gmail.com>
2023-04-22 05:27:02 +0900

    [add] message hooks

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Sidebar.scss';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
