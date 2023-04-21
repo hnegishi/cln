29817b2ab485be80b9df102c8d5dff79ceb9c3ff
hnegishi <hnegishimaru@gmail.com>
2023-04-22 03:46:04 +0900

    [fix] bug

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -9,11 +9,22 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import { auth, db } from '../../Firebase';
 import { useAppSelector } from '../../app/hooks';
 import useCollection from '../../hooks/useCollection';
+import { addDoc, collection } from 'firebase/firestore';
 
 
 const Sidebar = () => {
   const user = useAppSelector((state) => state.user);
   const { documents: channels } = useCollection("channels");
+
+  const addChannel = async () => {
+    let channelName: string | null = prompt("create new channel");
+
+    if (channelName) {
+      await addDoc(collection(db, "channels"), {
+        channelName: channelName,
+      });
+    }
+  };
 
   return (
     <div className="sidebar">
@@ -41,7 +52,7 @@ const Sidebar = () => {
               <ExpandMoreIcon />
               <h4>Text Channels</h4>
             </div>
-            <AddIcon className="sidebarAddIcon"/>
+            <AddIcon className="sidebarAddIcon" onClick={() => addChannel()}/>
           </div>
 
           <div className="sidebarChannelList">
